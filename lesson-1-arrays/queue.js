067876bacb532cea1772168c11f537b29da0ea86
kevchike <kevchike@gmail.com>
2020-09-09 18:03:57 +0100

    Implement a queue

---
+++ lesson-1-arrays/queue.js
@@ -0,0 +1,20 @@
+let queue = [];
+
+function enqueue(array, element) {
+	if (element == "") return array;
+
+	array.push(element);
+	return array;
+}
+
+function dequeue(array) {
+	array.shift();
+	return array;
+}
+
+queue = enqueue(queue, 2);
+queue = enqueue(queue, 5);
+queue = enqueue(queue, 7);
+queue = dequeue(queue);
+
+console.log(queue);
